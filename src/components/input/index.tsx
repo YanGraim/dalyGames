c7a1296f36d3c792814c5a9e02ad5f1efbf13f8a
YanGraim <yangraim@gmail.com>
2025-04-30 14:50:28 -0300

    função para pesquisa no component input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { FormEvent, useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { useRouter } from "next/navigation";
 
 export function Input() {
   const [input, setInput] = useState("");
+  const router = useRouter();
 
   function handleSearch(event: FormEvent) {
     event.preventDefault();
-    alert("digitou " + input);
+    if (input.trim() === "") return;
+    router.push(`/game/search/${input}`);
   }
 
   return (
