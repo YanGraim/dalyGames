b7d4028b8bd090329527a3e979c5bc446a12228f
YanGraim <yangraim@gmail.com>
2025-05-05 08:40:23 -0300

    adicona banner, title e description a pagina de details

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { Container } from "@/components/container";
+import { GameProps } from "@/utils/types/game";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 async function getId(id: string) {
@@ -16,14 +19,28 @@ export default async function GameDetail({
 }: {
   params: { id: string };
 }) {
-  const data = await getId(id);
+  const data: GameProps = await getId(id);
 
   if (!data) {
     redirect("/");
   }
   return (
-    <div>
-      <h1>Teste: {id}</h1>
-    </div>
+    <main className="w-full text-black">
+      <div className="bg-black h-80 sm:h-96 w-full relative">
+        <Image
+          src={data.image_url}
+          alt={data.title}
+          fill
+          priority
+          quality={100}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
+          className="object-cover w-full h-80 sm:h-96 opacity-70"
+        />
+      </div>
+      <Container>
+        <h1 className="text-2xl font-bold my-4">{data.title}</h1>
+        <p>{data.description}</p>
+      </Container>
+    </main>
   );
 }
